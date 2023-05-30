39b6394e01ca0477a83ddda87a8ddff479a2b782
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-31 00:51:56 +0300

    add carcass for SHA1 implementation

---
+++ lib/src/hash_algorithms/sha1.dart
@@ -0,0 +1,13 @@
+import 'dart:typed_data';
+
+import 'package:hash_functions/src/hash_algorithms/hash_algorithm.dart';
+
+
+///Implements SHA-1 algorithm
+class SHA1 extends HashAlgorithm {
+  @override
+  Uint8List process(Uint8List data) {
+    // TODO: implement process
+    throw UnimplementedError();
+  }
+}

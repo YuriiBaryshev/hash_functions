085574fe2f7a19f828da0f94b909e4f1c16a80c9
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-01 00:05:11 +0300

    add message padding procedure

--- lib/src/hash_algorithms/sha1.dart
+++ lib/src/hash_algorithms/sha1.dart
@@ -1,5 +1,4 @@
 import 'dart:typed_data';
-
 import 'package:hash_functions/src/hash_algorithms/hash_algorithm.dart';
 
 
@@ -9,5 +8,22 @@ class SHA1 extends HashAlgorithm {
   Uint8List process(Uint8List data) {
     // TODO: implement process
     throw UnimplementedError();
+  }
+
+
+  ///Pads data with standardised Merkle-Damgard enhanced construction
+  Uint8List pad(Uint8List data) {
+    BigInt originalLengthInBits = BigInt.from(data.length) << 3;
+
+    List<int> paddedData = [];
+    paddedData.addAll(data);
+    paddedData.add(0x80); //append '1'
+    paddedData.addAll(List.filled(56 - (paddedData.length & 0x3f), 0));
+    String hexInterpretationOfBitsLen = originalLengthInBits.toRadixString(16);
+    hexInterpretationOfBitsLen = hexInterpretationOfBitsLen.padLeft(16, '0');
+    for(int i = 0; i < 16; i += 2) {
+      paddedData.add(int.parse(hexInterpretationOfBitsLen.substring(i, i+2), radix: 16));
+    }
+    return Uint8List.fromList(paddedData);
   }
 }

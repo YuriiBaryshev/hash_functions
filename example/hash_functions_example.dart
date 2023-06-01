eb19cf11a2b0ce103e24780a01a493c033d4befb
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-01 22:18:08 +0300

    enhace example according to FIPS

--- example/hash_functions_example.dart
+++ example/hash_functions_example.dart
@@ -6,5 +6,7 @@ import 'package:hash_functions/src/hash_algorithms/hash_algorithm.dart';
 void main() {
   HashFunctionsFactory factory = HashFunctionsFactory();
   HashAlgorithm sha1 = factory.createSHA1();
-  print('sha1 of "": ${sha1.process(Uint8List.fromList([]))}'); //sha1 of "": da39a3ee5e6b4b0d3255bfef95601890afd80709
+  print('sha1 of "[0x61, 0x62, 0x63]": '
+      '${sha1.process(Uint8List.fromList([0x61, 0x62, 0x63]))}'
+  ); //sha1 of "[169, 153, 62, 54, 71, 6, 129, 106, 186, 62, 37, 113, 120, 80, 194, 108, 156, 208, 216, 157]"
 }

316b557d1bcc80bfaabcdb7de9788c3579a3a0fd
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-01 09:20:01 +0300

    update example

--- example/hash_functions_example.dart
+++ example/hash_functions_example.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:hash_functions/hash_functions.dart';
+import 'package:hash_functions/src/hash_algorithms/hash_algorithm.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  HashFunctionsFactory factory = HashFunctionsFactory();
+  HashAlgorithm sha1 = factory.createSHA1();
+  print('sha1 of "": ${sha1.process(Uint8List.fromList([]))}'); //sha1 of "": da39a3ee5e6b4b0d3255bfef95601890afd80709
 }

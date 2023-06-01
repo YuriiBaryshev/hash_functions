9ddb08e40f5f50db4614b2a1221131b04114d571
YuriiBaryshev <yura.baryshev@gmail.com>
2023-06-01 22:14:42 +0300

    add test vector test from FIPS

--- test/sha1_test.dart
+++ test/sha1_test.dart
@@ -25,5 +25,16 @@ void main() {
       ]);
       expect(sha1.pad(data), paddedData);
     });
+
+
+    test('processing test vectors', () {
+      Uint8List data = Uint8List.fromList([0x61, 0x62, 0x63]);
+      Uint8List hash = Uint8List.fromList([
+        0xa9, 0x99, 0x3e, 0x36, 0x47, 0x06, 0x81, 0x6a,
+        0xba, 0x3e, 0x25, 0x71, 0x78, 0x50, 0xc2, 0x6c,
+        0x9c, 0xd0, 0xd8, 0x9d
+      ]);
+      expect(sha1.process(data), hash);
+    });
   });
 }
